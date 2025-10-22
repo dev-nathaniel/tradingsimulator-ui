1935d18985607263d6fdda353e203cfbac1c192e
Olowodev <nathanielolowofoyeku@gmail.com>
2025-10-22 14:24:37 +0100

    layout children prop type

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ const geistMono = Geist_Mono({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children?: React.ReactElement<any, any>;
+  children: React.ReactNode;
 }>) {
   const pathname = usePathname();
   const [authModalOpen, setAuthModalOpen] = useState(false);
